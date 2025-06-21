12bed37af639ad08fe4d65546ea23c6516f15ab5
ersu095 <ersu095@gmail.com>
2025-06-21 11:05:32 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@ LOCAL_PATH := $(call my-dir)
 # if some modules are built directly from this directory (not subdirectories),
 # their rules should be written here.
 
-ifeq ($(TARGET_DEVICE),toro)
+ifeq ($(TARGET_DEVICE),S4R)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
